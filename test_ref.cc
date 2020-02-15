3a2a0d2445daa45ea0acf5382ef959527bb6a181
E.V.Eremin <e.vl.eremin@yandex.ru>
2020-02-15 23:28:12 +0300

    test_ref: better example

--- test_ref.cc
+++ test_ref.cc
@@ -11,12 +11,19 @@
 
 int main()
 {
-	int i = 314, j = 271;
-	int *pi = &i, *pj = &j;
-	int &ri = i, &rj = j;
+	int i = 314;
+	int *pi = &i;
+	int &ri = i;
 
-	printf("%i %i\n", ri, rj);
-	printf("%i %i\n", *pi, *pj);
+	printf("%i %i %i\n", i, ri, *pi);
+
+	ri = 271;
+
+	printf("%i %i %i\n", i, ri, *pi);
+
+	*pi = 161;
+
+	printf("%i %i %i\n", i, ri, *pi);
 
 	return 0;
 }
