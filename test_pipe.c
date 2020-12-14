00b5b3b0a895f04d6eed3145914cb505312470b6
E.V.Eremin <e.vl.eremin@yandex.ru>
2020-12-14 18:09:01 +0300

    pipe: refactor run_pipe_child()

--- test_pipe.c
+++ test_pipe.c
@@ -11,7 +11,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define TEST_PIPE_CHILD		"./test_pipe_child"
+#define PIPE_READ_FD		0
+#define PIPE_WRITE_FD		1
 
 #if 0
 static int read_buf(int fd, char *buf, int buf_size)
@@ -61,35 +62,9 @@ int write_buf(int fd, const char *buf, int buf_size)
 }
 #endif
 
-#define READ_FD		0
-#define WRITE_FD	1
-
-static int run_pipe_child(const char *child_path)
+static void ignore_sigchld(struct sigaction *oldsa)
 {
-	int retval = 0, child;
-	struct sigaction newsa, oldsa;
-
-	/* create a pipe for the messages */
-	int fds1[2];
-	if (pipe(fds1) != 0) {
-		printf("could not create a pipe 1\n");
-		return 0;
-	}
-
-	int fds2[2];
-	if (pipe(fds2) != 0) {
-		printf("could not create a pipe 2\n");
-		return 0;
-	}
-
-	/*
-	 * fds1[0] read parent
-	 * fds1[1] write child
-	 *
-	 * fds2[0] read child
-	 * fds2[1] write parent
-	 *
-	 */
+	struct sigaction newsa;
 
 	/*
 	 * This code arranges that the demise of the child does not cause
@@ -101,121 +76,200 @@ static int run_pipe_child(const char *child_path)
 	 */
 	memset(&newsa, '\0', sizeof(newsa));
 	newsa.sa_handler = SIG_DFL;
-	sigaction(SIGCHLD, &newsa, &oldsa);
+	sigaction(SIGCHLD, &newsa, oldsa);
+}
 
-	/* fork */
-	child = fork();
-	if (child == 0) {
-		static char *envp[] = { NULL };
-		const char *args[] = {
-			child_path,
-			"arg1",
-			"arg2",
-			NULL
-		};
+/*
+ * Reopen stdin, stdout, stderr as pipes in the child process and close
+ * unused fds.  (Child cannot use printf after stdout is redirected.)
+ */
+static void dup_stdio(int *fds1, int *fds2)
+{
+	close(fds1[PIPE_READ_FD]);
+	close(fds2[PIPE_WRITE_FD]);
 
-		close(fds1[READ_FD]);
-		close(fds2[WRITE_FD]);
-
-		/* reopen stdin as pipe */
-		if (dup2(fds2[READ_FD], STDIN_FILENO) != STDIN_FILENO) {
-			_exit(EXIT_FAILURE);
-		}
-
-		/* reopen stdout as pipe */
-		if (dup2(fds1[WRITE_FD], STDOUT_FILENO) != STDOUT_FILENO) {
-			_exit(EXIT_FAILURE);
-		}
-
-		/* reopen stderr as pipe */
-		if (dup2(fds1[WRITE_FD], STDERR_FILENO) != STDERR_FILENO) {
-			_exit(EXIT_FAILURE);
-		}
-
-		close(fds2[READ_FD]);
-		close(fds1[WRITE_FD]);
-
-		/* exec binary helper */
-		execve(child_path, (char *const *) args, envp);
-		//execl("/usr/bin/tee", "tee", (char*) NULL);
-
-		printf("%s helper binary execve failed: %m", __func__);
+	if (dup2(fds2[PIPE_READ_FD], STDIN_FILENO) != STDIN_FILENO) {
 		_exit(EXIT_FAILURE);
-	} else {
-		if (child > 0) {
-			int rc = 0;
-			close(fds2[READ_FD]);
-			close(fds1[WRITE_FD]);
-
-#if 1
-			char buf_i[128];
-
-			printf("%s reading %i ...\n", __func__, fds1[READ_FD]);
-			rc = read(fds1[READ_FD], buf_i, sizeof(buf_i) - 1);
-			if (rc > 0) {
-				buf_i[rc] = '\0';
-				printf("%s From child: '%s'\n", __func__, buf_i);
-			} else {
-				printf("%s read error %d: %m\n", __func__, rc);
-				retval = 1;
-			}
-#endif
-
-			char buf_o[] = "Hello from parent";
-			printf("%s writing from parent to %i...\n", __func__, fds2[WRITE_FD]);
-			rc = write(fds2[WRITE_FD], buf_o, sizeof(buf_o));
-			if (rc > 0) {
-				printf("%s Ok written %i to child\n", __func__, rc);
-			} else {
-				printf("%s write error %d: %m\n", __func__, rc);
-				retval = 1;
-			}
-
-			printf("%s reading %i ...\n", __func__, fds1[READ_FD]);
-			rc = read(fds1[READ_FD], buf_i, sizeof(buf_i) - 1);
-			if (rc > 0) {
-				buf_i[rc] = '\0';
-				printf("%s From child: '%s'\n", __func__, buf_i);
-			} else {
-				printf("%s read error %d: %m\n", __func__, rc);
-				retval = 1;
-			}
-
-			/* wait for helper to complete: */
-			printf("%s waiting to complete...\n", __func__);
-			while ((rc = waitpid(child, &retval, 0)) < 0 && errno == EINTR);
-			if (rc < 0) {
-				printf("%s unix_chkpwd waitpid returned %d: %m", __func__, rc);
-				retval = 1;
-			} else if (!WIFEXITED(retval)) {
-				printf("%s unix_chkpwd abnormal exit: %d", __func__, retval);
-				retval = 1;
-			} else {
-				retval = WEXITSTATUS(retval);
-			}
-		} else {
-			printf("%s Fork failed: %m\n", __func__);
-			retval = 1;
-		}
-		close(fds1[READ_FD]);
-		close(fds2[WRITE_FD]);
 	}
 
-	/* restore old signal handler */
-	sigaction(SIGCHLD, &oldsa, NULL);
+	if (dup2(fds1[PIPE_WRITE_FD], STDOUT_FILENO) != STDOUT_FILENO) {
+		_exit(EXIT_FAILURE);
+	}
 
-	printf("%s Returning %d\n", __func__, retval);
-	return retval;
+	if (dup2(fds1[PIPE_WRITE_FD], STDERR_FILENO) != STDERR_FILENO) {
+		_exit(EXIT_FAILURE);
+	}
+
+	close(fds2[PIPE_READ_FD]);
+	close(fds1[PIPE_WRITE_FD]);
+}
+
+static int talk_to_child(int *fds)
+{
+
+	int r;
+	const int buf_size = 128;
+#if 1
+	char buf_i[buf_size];
+
+	printf("%s reading %i ...\n", __func__, fds[PIPE_READ_FD]);
+	r = read(fds[PIPE_READ_FD], buf_i, buf_size);
+	if (r > 0) {
+		buf_i[r] = '\0';
+		printf("%s From child: '%s'\n", __func__, buf_i);
+	} else {
+		printf("%s read error %d: %s\n", __func__,
+			r, strerror(errno));
+		return r;
+	}
+#endif
+
+	char buf_o[] = "Hello from parent";
+	printf("%s writing from parent to %i...\n", __func__, fds[PIPE_WRITE_FD]);
+	r = write(fds[PIPE_WRITE_FD], buf_o, sizeof(buf_o));
+	if (r > 0) {
+		printf("%s Ok written %i to child\n", __func__, r);
+	} else {
+		printf("%s write error %d: %s\n", __func__,
+			r, strerror(errno));
+		return r;
+	}
+
+	printf("%s reading %i ...\n", __func__, fds[PIPE_READ_FD]);
+	r = read(fds[PIPE_READ_FD], buf_i, buf_size);
+	if (r > 0) {
+		buf_i[r] = '\0';
+		printf("%s From child: '%s'\n", __func__, buf_i);
+	} else {
+		printf("%s read error %i: %s\n", __func__,
+			r, strerror(errno));
+		return r;
+	}
+
+	return 0;
+}
+
+int wait_and_fini(int child, int *fds)
+{
+	int r, status;
+
+	/* wait for helper to complete: */
+	printf("%s waiting %i to complete...\n", __func__, child);
+	while ((r = waitpid(child, &status, 0)) < 0 && errno == EINTR);
+	if (r < 0) {
+		printf("%s unix_chkpwd waitpid returned %d: %s", __func__,
+			r, strerror(errno));
+		r = EXIT_FAILURE;
+	} else if (!WIFEXITED(status)) {
+		printf("%s unix_chkpwd abnormal exit: %d", __func__, r);
+		r = EXIT_FAILURE;
+	} else {
+		r = WEXITSTATUS(status);
+	}
+
+	close(fds[PIPE_READ_FD]);
+	close(fds[PIPE_WRITE_FD]);
+
+	return r;
+}
+
+static int run_pipe_child(const char *child_path,
+			  const char *child_argv[],
+			  const char *child_envp[],
+			  int *fds,
+			  struct sigaction *oldsa)
+{
+	int child;
+	int fds1[2];
+	int fds2[2];
+
+	/*
+	 * The ends of pipes are used as follows:
+	 *
+	 * fds1[0] read from parent
+	 * fds1[1] write to child
+	 *
+	 * fds2[0] read from child
+	 * fds2[1] write to parent
+	 *
+	 */
+
+	if (pipe(fds1) != 0) {
+		printf("%s could not create a pipe fds1: %s\n",
+			__func__, strerror(errno));
+		return 0;
+	}
+
+	if (pipe(fds2) != 0) {
+		printf("%s could not create a pipe fds2: %s\n",
+			__func__, strerror(errno));
+		return 0;
+	}
+
+	ignore_sigchld(oldsa);
+
+	child = fork();
+	if (child < 0) {
+		printf("%s Fork failed: %s\n", __func__, strerror(errno));
+		close(fds1[PIPE_READ_FD]);
+		close(fds1[PIPE_WRITE_FD]);
+		close(fds2[PIPE_READ_FD]);
+		close(fds2[PIPE_WRITE_FD]);
+		return 1;
+	}
+
+	if (child == 0) {
+		dup_stdio(fds1, fds2);
+
+		execve(child_path,
+			(char *const *) child_argv,
+			(char * const *)child_envp);
+		//execl("/usr/bin/tee", "tee", (char*) NULL);
+
+		printf("%s helper binary execve failed: %s", __func__,
+			strerror(errno));
+		_exit(EXIT_FAILURE);
+	} else {
+		close(fds2[PIPE_READ_FD]);
+		close(fds1[PIPE_WRITE_FD]);
+
+		fds[PIPE_READ_FD] = fds1[PIPE_READ_FD];
+		fds[PIPE_WRITE_FD] = fds2[PIPE_WRITE_FD];
+	}
+
+	printf("%s Returning child %i\n", __func__, child);
+	return child;
 }
 
 //int main(int argc, char **argv)
 int main()
 {
-	int r;
+	int fds[2];
+	int child;
+	const char *child_path = "./test_pipe_child";
+	const char *child_argv[] = {
+		child_path,
+		"arg1",
+		"arg2",
+		NULL
+	};
+	const char *child_envp[] = {
+		"VAR1=val1",
+		"VAR2=val2",
+		NULL
+	};
+	struct sigaction oldsa;
 
 	printf("Parent uid %i, euid %i\n", getuid(), geteuid());
 
-	r = run_pipe_child(TEST_PIPE_CHILD);
+	child = run_pipe_child(child_path, child_argv, child_envp, fds, &oldsa);
 
-	return r == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+	talk_to_child(fds);
+
+	wait_and_fini(child, fds);
+
+	/* restore old signal handler */
+	sigaction(SIGCHLD, &oldsa, NULL);
+
+	return child == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
