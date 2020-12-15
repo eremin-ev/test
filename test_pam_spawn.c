8df21da1ea7f76213b3c62950fb1b0a8f9e51fdc
E.V.Eremin <e.vl.eremin@yandex.ru>
2020-12-15 17:03:24 +0300

    pam: parent talks with spawned child
    Refactoring is required though.

--- test_pam_spawn.c
+++ test_pam_spawn.c
@@ -14,17 +14,177 @@
 
 #include "spawn-child.h"
 
+enum reply_type {
+	REP_PASSWD =		1U << 0,
+	REP_PASSWD_CURRENT =	1U << 1,
+	REP_PASSWD_NEW =	1U << 2,
+	REP_PASSWD_RETYPE =	1U << 3,
+};
+
 #define TEST_PAM_HELPER		"./test_pam_helper"
+
+struct pam_msg_rq {
+	int msg_style;
+	int msg_len;
+	int rq_flags;
+	char *msg;
+};
+
+struct pam_msg_rp {
+	int in_len;
+	char *in_str;
+};
+
+static const char *msg_type_str(int t)
+{
+	const char *s = "<unknown>";
+
+	switch (t) {
+	case PAM_PROMPT_ECHO_OFF: s = "PAM_PROMPT_ECHO_OFF"; break;
+	case PAM_PROMPT_ECHO_ON: s = "PAM_PROMPT_ECHO_ON"; break;
+	case PAM_ERROR_MSG: s = "PAM_ERROR_MSG"; break;
+	case PAM_TEXT_INFO: s = "PAM_TEXT_INFO"; break;
+	}
+
+	return s;
+}
+
+int read_from_stdin(struct pam_msg_rp *rp)
+{
+	int r;
+	int tok_len = 128;
+	char *tok_str = malloc(tok_len);
+	r = read(0, tok_str, tok_len);
+	if (r == -1) {
+		printf("%s: %s\n", __func__, strerror(errno));
+		return 0;
+	}
+	tok_str[r] = '\0';
+
+	rp->in_len = r;
+	rp->in_str = tok_str;
+
+	printf("%s r %i, authtok '%s'\n", __func__, rp->in_len, rp->in_str);
+
+	return r;
+}
+
+int rq_handle(struct pam_msg_rq *rq)
+{
+	int rep_flags = 0;
+	printf("%s %s '%s:%i'\n", __func__,
+		msg_type_str(rq->msg_style), rq->msg, rq->msg_len);
+	if (rq->msg_style == PAM_PROMPT_ECHO_OFF &&
+			!strcmp(rq->msg, "Password: ")) {
+		rep_flags |= REP_PASSWD;
+	}
+
+	if (rq->msg_style == PAM_PROMPT_ECHO_OFF &&
+			!strcmp(rq->msg, "Current password: ")) {
+		rep_flags |= REP_PASSWD_RETYPE;
+	}
+
+	if (rq->msg_style == PAM_PROMPT_ECHO_OFF &&
+			!strcmp(rq->msg, "New password: ")) {
+		rep_flags |= REP_PASSWD_NEW;
+	}
+
+	if (rq->msg_style == PAM_PROMPT_ECHO_OFF &&
+			!strcmp(rq->msg, "Retype new password: ")) {
+		rep_flags |= REP_PASSWD_RETYPE;
+	}
+
+	rq->rq_flags = rep_flags;
+
+	return rep_flags;
+}
+
+int msg_read(int fd, struct pam_msg_rq *rq)
+{
+	int r;
+
+	read(fd, &rq->msg_style, sizeof(rq->msg_style));
+	read(fd, &rq->msg_len, sizeof(rq->msg_len));
+	rq->msg = malloc(rq->msg_len);
+	r = read(fd, rq->msg, rq->msg_len);
+	rq->msg[r] = '\0';
+
+	printf("%s msg_style %i, msg '%s:%i'\n", __func__,
+		rq->msg_style,
+		rq->msg, rq->msg_len);
+
+	return r;
+}
+
+static int rq_input_required(unsigned flags)
+{
+	return flags & REP_PASSWD
+			|| flags & REP_PASSWD_CURRENT
+			|| flags & REP_PASSWD_NEW
+			|| flags & REP_PASSWD_RETYPE;
+}
 
 static int talk_to_child(int *fds)
 {
+	int i, r;
+	int num_msg, input_required;
+	struct pam_msg_rq *rq;
+	struct pam_msg_rp rp;
 
+	do {
+		input_required = 0;
+
+		printf("%s reading %i ...\n", __func__, fds[PIPE_READ_FD]);
+		read(fds[PIPE_READ_FD], &num_msg, sizeof(num_msg));
+
+		rq = malloc(num_msg * sizeof(struct pam_msg_rq));
+
+		for (i = 0; i < num_msg; i++) {
+			msg_read(fds[PIPE_READ_FD], &rq[i]);
+			if (rq_handle(&rq[i])) {
+				++input_required;
+			}
+		}
+
+		if (input_required == num_msg) {
+			r = write(fds[PIPE_WRITE_FD], &num_msg, sizeof(num_msg));
+			printf("%s OK r %d write %d to child %d\n", __func__,
+				r, num_msg, fds[PIPE_READ_FD]);
+
+			/* obtain input for each request */
+			for (i = 0; i < num_msg; i++) {
+				if (rq_input_required(rq[i].rq_flags)) {
+					if (read_from_stdin(&rp)) {
+						r = write(fds[PIPE_WRITE_FD], &rp.in_len, sizeof(rp.in_len));
+						r = write(fds[PIPE_WRITE_FD], rp.in_str, rp.in_len);
+					} else {
+						printf("%s Cannot write to child\n", __func__);
+					}
+				} else {
+					printf("%s Unhandled req\n", __func__);
+				}
+			}
+		} else if (input_required == 0) {
+			r = write(fds[PIPE_WRITE_FD], &input_required, sizeof(input_required));
+			printf("%s OK r %d write input_required %d to child %d\n", __func__,
+				r, input_required, fds[PIPE_READ_FD]);
+		} else {
+			printf("%s bad input_required %d\n", __func__, input_required);
+		}
+
+		for (i = 0; i < num_msg; i++) {
+			free(rq[i].msg);
+		}
+
+		free(rq);
+
+	} while (num_msg);
+
+#if 0
 	int r;
 	const int buf_size = 128;
-#if 1
 	char buf_i[buf_size];
 
-	printf("%s reading %i ...\n", __func__, fds[PIPE_READ_FD]);
 	r = read(fds[PIPE_READ_FD], buf_i, buf_size);
 	if (r > 0) {
 		buf_i[r] = '\0';
