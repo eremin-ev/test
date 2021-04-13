e3ec09dab386148966d73c95fc857e9e7e72f556
Eremin Evgeny <e.vl.eremin@yandex.ru>
2021-04-14 01:46:30 +0300

    test_sizeof: add off_t and off64_t

--- test_sizeof.c
+++ test_sizeof.c
@@ -3,6 +3,17 @@
  * Version 2.  See the file COPYING for more details.
  */
 
+/*
+ * Add the following line to inspect code generated for different
+ * machines for foo_int() and foo_size_t()
+ *
+ *	test_sizeof: CFLAGS += -save-temps
+ *
+ */
+
+#define _LARGEFILE64_SOURCE
+#define _FILE_OFFSET_BITS 64
+
 #include <stdio.h>
 
 #define TEST_STRING_1	"A test string"
@@ -33,25 +44,14 @@ size_t foo_size_t(size_t a, size_t b)
 int main(void)
 {
 	printf("sizeof(\"" TEST_STRING_1 "\"): %zi\n", sizeof(TEST_STRING_1));
-
 	printf("sizeof(\"" TEST_STRING_2 "\"): %zi\n", sizeof(TEST_STRING_2));
-
 	printf("sizeof(\"" TEST_STRING_3 "\"): %zi\n", sizeof(TEST_STRING_3));
-
 	//printf("sizeof('" TEST_CHAR "'): %zi\n", sizeof(TEST_CHAR));
-
-
 	printf("sizeof(int)\t%zi\n", sizeof(int));
 	printf("sizeof(long)\t%zi\n", sizeof(long));
 	printf("sizeof(size_t)\t%zi\n", sizeof(size_t));
-
-	/*
-	 * Add the following line to inspect code generated for different
-	 * machines for foo_int() and foo_size_t()
-	 *
-	 *	test_sizeof: CFLAGS += -save-temps
-	 *
-	 */
+	printf("sizeof(off_t)\t%zi\n", sizeof(off_t));
+	printf("sizeof(off64_t)\t%zi\n", sizeof(off64_t));
 
 	printf("int\t%i\n", foo_int(337, 773));
 	printf("size_t\t%zi\n", foo_size_t(337, 773));
