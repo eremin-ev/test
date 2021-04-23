5a4705833e27aac37f93e9b13d355e08aacadcb3
Eremin Evgeny <e.vl.eremin@yandex.ru>
2021-04-24 00:05:46 +0300

    modelview: update cell using timer

--- modelview/mymodel.cc
+++ modelview/mymodel.cc
@@ -1,8 +1,14 @@
+#include <QTime>
+
 #include "mymodel.h"
 
 MyModel::MyModel(QObject *parent)
     : QAbstractTableModel(parent)
+    , timer(new QTimer(this))
 {
+    timer->setInterval(1000);
+    connect(timer, &QTimer::timeout , this, &MyModel::timerHit);
+    timer->start();
 }
 
 int MyModel::rowCount(const QModelIndex & /*parent*/) const
@@ -17,10 +23,28 @@ int MyModel::columnCount(const QModelIndex & /*parent*/) const
 
 QVariant MyModel::data(const QModelIndex &index, int role) const
 {
-    if (role == Qt::DisplayRole)
-    	return QString("Row%1, Column%2")
-                   .arg(index.row() + 1)
-                   .arg(index.column() +1);
+    int row = index.row();
+    int col = index.column();
+
+    printf("%s %i %i %i\n", __PRETTY_FUNCTION__, row, col, role);
+
+    if (role == Qt::DisplayRole) {
+        if (!row && !col) {
+            return QTime::currentTime().toString();
+        } else {
+            return QString("Row%1, Column%2")
+                        .arg(index.row() + 1)
+                        .arg(index.column() +1);
+        }
+    }
 
     return QVariant();
+}
+
+void MyModel::timerHit()
+{
+    //we identify the top left cell
+    QModelIndex topLeft = createIndex(0,0);
+    //emit a signal to make the view reread identified data
+    emit dataChanged(topLeft, topLeft, {Qt::DisplayRole});
 }
