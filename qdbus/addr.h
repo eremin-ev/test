b59614f885ea9c3c229c42e7ef4af2b274e58024
Eremin Evgeny <e.vl.eremin@yandex.ru>
2021-05-11 01:53:48 +0300

    qdbus: common header for service/path/interface address info

---
+++ qdbus/addr.h
@@ -0,0 +1,8 @@
+#ifndef ADDR_H
+#define ADDR_H
+
+#define CALC_DBUS_SERVICE       "my.test.Calc_Service"
+#define CALC_DBUS_OBJECT_PATH   "/my/test/Calc_Object"
+#define CALC_DBUS_INTERFACE     "my.test.Calc_Interface"
+
+#endif  /* ADDR_H */
