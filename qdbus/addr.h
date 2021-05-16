6c58c27a8084da9b833ea707f879487a29cd824f
Eremin Evgeny <e.vl.eremin@yandex.ru>
2021-05-17 02:21:20 +0300

    qdbus: initial p2p support

--- qdbus/addr.h
+++ qdbus/addr.h
@@ -1,8 +1,9 @@
 #ifndef ADDR_H
 #define ADDR_H
 
-#define CALC_DBUS_SERVICE       "my.test.Calc_Service"
-#define CALC_DBUS_OBJECT_PATH   "/my/test/Calc_Object"
-#define CALC_DBUS_INTERFACE     "my.test.Calc_Interface"
+#define SERV_DBUS_SERVICE       "my.test.Serv_Service"
+#define SERV_DBUS_OBJECT_PATH   "/my/test/Serv_Object"
+#define SERV_DBUS_INTERFACE     "my.test.Serv_Interface"
+#define SERV_DBUS_P2P_ADDRESS   "unix:path=/tmp/qdbus-serv-socket"
 
 #endif  /* ADDR_H */
