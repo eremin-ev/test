403ecd752ac53f91333c05716ee9442a8d53d995
Eremin Evgeny <e.vl.eremin@yandex.ru>
2023-03-30 14:25:37 +0300

    test_member: options cache template

--- test_member.cc
+++ test_member.cc
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <variant>
 
 struct A {
     A();
@@ -16,14 +17,43 @@ struct A {
     void factor1(int factor);
     void factor2(int factor);
 
+    using ptr_to_member = void (A::*)(int param);
+
 private:
     int m_data1;
     int m_data2;
 };
 
-using ptr_to_member = void (A::*)(int param);
+template <typename T, typename V>
+struct Option {
+    using SetOption = bool (T::*)(const V &value);
+    SetOption setOption;
+    V value;
+};
 
-const std::map <std::string, ptr_to_member> setter_map = {
+template <typename T, typename V>
+using Options = std::map<std::string, Option<T, V>>;
+
+struct SomeData {
+    using ValueType = std::variant<std::monostate, int, std::string>;
+
+    const static std::string Value1;
+    const static std::string Value2;
+
+    ValueType getOption(const std::string &key);
+    bool setOption(const std::string &key, const ValueType &value);
+    void dumpState();
+
+    bool setValue1(const ValueType &value);
+    bool setValue2(const ValueType &value);
+
+    Options<SomeData, ValueType> &cache;
+
+    int value1;
+    std::string value2;
+};
+
+const std::map <std::string, A::ptr_to_member> setter_map = {
     { "set1", &A::set1 },
     { "set2", &A::set2 },
     { "factor1", &A::factor1 },
@@ -63,6 +93,61 @@ void A::factor2(int factor)
     m_data2 *= factor;
 }
 
+const std::string SomeData::Value1 = "value1";
+const std::string SomeData::Value2 = "value2";
+
+bool SomeData::setValue1(const ValueType &value)
+{
+    value1 = std::get<int>(value);
+
+    return true;
+}
+
+bool SomeData::setValue2(const ValueType &value)
+{
+    value2 = std::get<std::string>(value);
+
+    return true;
+}
+
+SomeData::ValueType SomeData::getOption(const std::string &key)
+{
+    auto option = cache.find(key);
+    if (option == cache.end()) {
+        return std::monostate{};
+    }
+
+    return option->second.value;
+}
+
+bool SomeData::setOption(const std::string &key,
+                         const ValueType &value)
+{
+    auto option = cache.find(key);
+    if (option == cache.end()) {
+        return false;
+    }
+
+    if (option->second.value == value) {
+        return true;
+    }
+
+    Option<SomeData, ValueType>::SetOption setFunc = option->second.setOption;
+    if (setFunc && !(this->*setFunc)(value)) {
+        return false;
+    }
+
+    option->second.value = value;
+
+    return true;
+}
+
+void SomeData::dumpState()
+{
+    std::cout << __func__ << " " << SomeData::Value1 << " " << value1 << '\n';
+    std::cout << __func__ << " " << SomeData::Value2 << " '" << value2 << "'\n";
+}
+
 static void test_ptr(A *a, const std::string &name, int param)
 {
     const auto i = setter_map.find(name);
@@ -71,8 +156,8 @@ static void test_ptr(A *a, const std::string &name, int param)
         return;
     }
 
-    const ptr_to_member p1 = i->second;
-    const ptr_to_member p2 = nullptr;
+    const A::ptr_to_member p1 = i->second;
+    const A::ptr_to_member p2 = nullptr;
 
     std::cout << __func__ << " p1 " << p1 << ", p2 " << p2 << '\n';
 
@@ -81,7 +166,7 @@ static void test_ptr(A *a, const std::string &name, int param)
     (a->*p1)(param);
 }
 
-int main()
+static void test1()
 {
     A a;
 
@@ -90,6 +175,45 @@ int main()
     test_ptr(&a, "set1", 5);
     test_ptr(&a, "factor2", 7);
     test_ptr(&a, "factor5", 11);
+}
 
+static void test2()
+{
+    Options<SomeData, SomeData::ValueType> someDataOptions = {
+        {
+            SomeData::Value1, {
+                .setOption = &SomeData::setValue1,
+                .value = -1,
+            },
+        }, {
+            SomeData::Value2, {
+                .setOption = &SomeData::setValue2,
+                .value = std::string{"null"},
+            },
+        }
+    };
+
+    SomeData d {
+        .cache = someDataOptions,
+        .value1 = std::get<int>(someDataOptions[SomeData::Value1].value),
+        .value2 = std::get<std::string>(someDataOptions[SomeData::Value2].value),
+    };
+
+    d.dumpState();
+    d.setOption(SomeData::Value1, 123);
+    d.setOption(SomeData::Value2, "abc");
+    d.dumpState();
+
+    int value1_1 = std::get<int>(someDataOptions[SomeData::Value1].value);
+    std::cout << __func__ << " " << SomeData::Value1 << " v1 " << value1_1 << '\n';
+
+    int value1 = std::get<int>(d.getOption(SomeData::Value1));
+    std::cout << __func__ << " " << SomeData::Value1 << " v2 " << value1 << '\n';
+}
+
+int main()
+{
+    //test1();
+    test2();
     return 0;
 }
