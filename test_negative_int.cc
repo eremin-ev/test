0d42e4bc6ee13cb835b1d38e2df689770871ffc3
Eremin Evgeny <e.vl.eremin@yandex.ru>
2024-02-22 01:15:52 +0300

    test_negative_int: update description, add check 4

--- test_negative_int.cc
+++ test_negative_int.cc
@@ -3,6 +3,7 @@
  * Version 2.  See the file COPYING for more details.
  */
 
+#include <cstdint>
 #include <iostream>
 
 int main()
@@ -11,22 +12,28 @@ int main()
     int8_t b = 0b10100011;  // two-complement
 
     // check 1: substract n-bit number from a 2^n
-    int8_t c1 = 0b100000000 - a;
+    int8_t c1 = static_cast<int8_t>((1U<<8*sizeof(c1)) - a);
 
-    // check 2: a simpler/faster equivalent of the subtraction:
-    //          invert all bits and add 1
+    std::cout << sizeof(c1) << '\n';
+
+    // check 2: 2^N-complement or just two's complement --
+    //          a simpler/faster equivalent of the subtraction:
+    //          invert all bits and add 1 so that the sum of
+    //          these numbers is always equal to 2^N
     int8_t c2 = ~a + 1;
 
     // check 3
     int8_t c3 = -a;
 
-    int ia = a;
-    int ib = b;
+    unsigned ia = static_cast<uint8_t>(a);
+    unsigned ib = static_cast<uint8_t>(c2);
 
     std::cout << ia << ", negative is " << ib
               << ", check 1: " << (b == c1 ? "Ok" : "failed")
               << ", check 2: " << (b == c2 ? "Ok" : "failed")
-              << ", check 3: " << (b == c3 ? "Ok" : "failed") << '\n';
+              << ", check 3: " << (b == c3 ? "Ok" : "failed")
+              << ", check 4: " << ((ia + ib) == (1U<<8*sizeof(c2)) ? "Ok" : "failed")
+              << '\n';
 
     return 0;
 }
