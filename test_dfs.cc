e51cdfa6149faab128eb4c66e9138b11a727b49a
Eremin Evgeny <e.vl.eremin@yandex.ru>
2024-03-04 00:06:23 +0300

    fix dfs: find paths from src to dst

--- test_dfs.cc
+++ test_dfs.cc
@@ -2,6 +2,20 @@
  * This source code is licensed under the GNU General Public License,
  * Version 2.  See the file COPYING for more details.
  *
+ * Depth First Search or DFS for a Graph
+ * https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
+ *
+ * Introduction to Depth First Search Algorithm (DFS)
+ * https://www.baeldung.com/cs/depth-first-search-intro
+ *
+ * Find All Simple Paths Between Two Vertices in a Graph
+ * https://www.baeldung.com/cs/simple-paths-between-two-vertices
+ *
+ *
+ * Finally:
+ *
+ * Non recursive DFS algorithm for simple paths between two points
+ * https://stackoverflow.com/questions/35170956/non-recursive-dfs-algorithm-for-simple-paths-between-two-points
  *
  */
 
@@ -68,7 +82,84 @@ public:
         }*/
     }
 
+    void dfs_from_to(const std::vector<std::vector<int>> &graph, int src, int dst)
+    {
+        if (!graph.size()) {
+            return;
+        }
+
+        //int v_idx = 0;
+        std::vector<bool> visited(graph.size());
+        //std::vector<int> stack;
+        std::vector<Vertex> stack;
+        std::vector<int> path;
+
+        stack.push_back({src, 0});
+        path.push_back(src);
+
+        std::cout << __func__ << "   visited " << ": " << show(visited) << '\n';
+        std::cout << __func__ << "   stack " << ": " << show(stack) << '\n';
+
+        while (!stack.empty()) {
+            Vertex &v = stack.back();
+
+            //std::cout << __func__ << " " << v.idx << '\n';
+
+            // if found destination or no more neighbours to eplore
+            if (v.idx == dst || v.neighbour == (int)graph[v.idx].size()) {
+                // yes, once again same if...
+                if (v.idx == dst) {
+                    std::cout << __func__ << " found path from " << src
+                                          << " to " << dst
+                                          << ": " << show(path) << '\n';
+                }
+
+                visited[v.idx] = false;
+                if (path.size() == 0) {
+                    std::cout << __func__ << " attempt to pop from an empty path\n";
+                    abort();
+                }
+                path.pop_back();
+                stack.pop_back();
+            } else {
+                int next_neighbour = graph[v.idx][v.neighbour];
+                ++v.neighbour;
+                if (!visited[next_neighbour]) {
+                    visited[next_neighbour] = true;
+                    stack.push_back({next_neighbour, 0});
+                    path.push_back(next_neighbour);
+                }
+            }
+
+            //std::cout << __func__ << "   visited " << ": " << show(visited) << '\n';
+            //std::cout << __func__ << "   stack for " << v.idx << ": " << show(stack) << '\n';
+            //std::cout << __func__ << "   path for " << v.idx << ": " << show(path) << '\n';
+        }
+    }
+
 private:
+    struct Vertex {
+        int idx;
+        int neighbour;  // neighbour index
+    };
+
+    std::string show(const std::vector<Vertex> &v) const
+    {
+        std::string o;
+        o += '(';
+        for (const auto &e : v) {
+            o += '{';
+            o += std::to_string(e.idx);
+            o += ',';
+            o += std::to_string(e.neighbour);
+            o += '}';
+            o += ',';
+        }
+        o += ')';
+
+        return o;
+    }
+
     std::string show(const std::vector<int> &v) const
     {
         std::string o;
@@ -111,9 +202,17 @@ static int test_dfs()
         std::vector<std::vector<int>> g;
         //uint32_t b;
     } cases[] = {
-        { .g = {{1,2}, {3}, {3}, {}}, },
-        { .g = {{1,2,3}, {}, {3,4}, {}, {}}, },
-        { .g = {{1,2}, {2,3}, {3,4}, {4}, {3}}, },
+        //{ .g = {{1,2}, {3}, {3}, {}, }, },
+        //{ .g = {{1,2,3}, {}, {3,4}, {}, {}, }, },
+        //           0        1    2   3   4
+        //{ .g = {{1,3,4}, {2,3,4}, {3}, {}, {}, }, },
+        //           0        1    2    3   4
+        //{ .g = {{1,3,4}, {2,3,4}, {3}, {4}, {}, }, },
+        //{ .g = {{1,2}, {2,3}, {3,4}, {4}, {3}, }, },
+        //           0    1    2    3    4     5    6     7    8     9   10
+        { .g = {{1,2,3}, {4}, {6}, {8}, {5}, {10}, {7}, {10}, {9}, {10}, {} }, },
+        //       0        1    2    3    4     5    6    7   8
+        //{ .g = {{1}, {2,3,4}, {5}, {6}, {7},  {8}, {8}, {}, {}, }, },
     };
 
     int errors = 0;
@@ -122,7 +221,8 @@ static int test_dfs()
     for (const auto &c : cases) {
         int r = 0;
         //s.all_paths(c.g);
-        s.dfs(c.g);
+        //s.dfs(c.g);
+        s.dfs_from_to(c.g, 0, c.g.size() - 1);
         std::cout << (r ? "ok" : "failed") << " "
                   << '\n';
         errors += !r;
